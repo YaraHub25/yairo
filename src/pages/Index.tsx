e90669a54c2bc3ae9750111e44caf65061b76556
Yara <Yaralili15@gmail.com>
2025-12-27 22:11:24 -0700

    fixed index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,18 +3,22 @@ import HeroSection from "@/components/HeroSection";
 import LiveDemoSection from "@/components/LiveDemoSection";
 import InsightsSection from "@/components/InsightsSection";
 import BusinessSection from "@/components/BusinessSection";
+import FinalCTASection from "@/components/FinalCTASection";
 import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
+
       <main>
         <HeroSection />
-        <LiveDemoSection />
         <InsightsSection />
+        <LiveDemoSection />
         <BusinessSection />
+        <FinalCTASection />
       </main>
+
       <Footer />
     </div>
   );
