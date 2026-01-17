db02a47cd6e2fff06c64b4c7d01fd524cce972ab
Yara <Yaralili15@gmail.com>
2026-01-16 21:32:18 -0700

    added InsightsSection into index.tsx

--- yairo-mobile/app/(tabs)/index.tsx
+++ yairo-mobile/app/(tabs)/index.tsx
@@ -2,6 +2,9 @@ import { ScrollView, StyleSheet } from "react-native";
 import HeroSection from "@/components/HeroSection";
 import DemoCard from "@/components/DemoCard";
 import SearchSection from "@/components/SearchSection";
+import InsightsSection from "@/components/InsightsSection";
+
+<InsightsSection />
 
 export default function HomeScreen() {
   return (
